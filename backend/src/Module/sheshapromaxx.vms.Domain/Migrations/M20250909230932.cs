10f7c3bdf5e0d46044081f4fd5d5814bc83cc7eb
Ibango7 <63455764+Ibango7@users.noreply.github.com>
2025-08-15 10:51:13 +0200

    Add migrations M20250909230932 and M20250909230933: establish foreign key for OrderId in vms_Vouchers and rename columns Status to StatusLkp and PaymentStatusRef to PaymentStatusRefLkp.

---
+++ backend/src/Module/sheshapromaxx.vms.Domain/Migrations/M20250909230932.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentMigrator;
+using Shesha.FluentMigrator;
+using Shesha.Domain;
+
+namespace sheshapromaxx.vms.Domain.Migrations
+{
+    [Migration(20250909230932)]
+    public class M20250909230932 : OneWayMigration
+    {
+        public override void Up()
+        {
+            // Just ensure the foreign key constraint is correct
+            // Simply delete the OrderId column - this will automatically drop any constraints
+            //Delete.Column("OrderId").FromTable("vms_Vouchers");
+            Alter.Table("vms_Vouchers")
+               .AddForeignKeyColumn("OrderId", "vms_Orders").Nullable();
+
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/src/Module/sheshapromaxx.vms.Domain/Migrations/M20250909230933.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentMigrator;
+using Shesha.FluentMigrator;
+using Shesha.Domain;
+
+namespace sheshapromaxx.vms.Domain.Migrations
+{
+    [Migration(20250909230933)]
+    public class M20250909230933 : OneWayMigration
+    {
+        public override void Up()
+        {
+           
+    
+            // Rename Status to StatusLkp
+            Rename.Column("Status").OnTable("vms_Vouchers").To("StatusLkp");
+            // Rename PaymentStatusRef to PaymentStatusRefLkp  
+            Rename.Column("PaymentStatusRef").OnTable("vms_Vouchers").To("PaymentStatusRefLkp");
+        
+
+    }
+
+    public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
